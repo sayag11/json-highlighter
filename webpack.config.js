f83c8a25e3014bf089e0669b60cf3e5e4e35d4e7
Yehonatan <yehonatan.sayag@xmcyber.com>
2021-07-10 18:07:18 +0300

    remove exclude: /node_modules/, from webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,6 @@ module.exports = {
             {
                 test: /\.js$|jsx/,
                 include: path.resolve(__dirname, 'src'),
-                exclude: /node_modules/,
                 use: [{
                     loader: 'babel-loader',
                     options: {
